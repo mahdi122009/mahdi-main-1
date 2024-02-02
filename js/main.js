5ecfb70b0472b416ca73d0ce8616cdf3b1795ead
primkulov mahdi <primkulovmahdi7@gmail.com>
2024-02-02 15:14:27 +0500

    денги

--- js/main.js
+++ js/main.js
@@ -222,3 +222,22 @@ document.getElementById('myForm').addEventListener('submit', function(e) {
     console.log('Телефон:', phone);
     console.log('Комментарий:', comment);
 });
+
+
+
+        // Get the modal
+        var modal = document.getElementById('id01');
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function (event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+
+
+
+
+
+
+
